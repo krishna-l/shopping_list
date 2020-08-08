a95c39cb7799e43b118992a3df7f7e37ba269b74
Krishna,L <krishna.lk1911@gmail.com>
2020-08-08 20:05:53 +0530

    Implemented JWT auth

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 //item model
 const Item = require('../../model/Items');
 
@@ -7,17 +8,17 @@ const Item = require('../../model/Items');
 // @desc Get all items
 // @access public
 
-router.get('/', (req, res)=> {
+router.get('/', (req, res) => {
     Item.find()
-        .sort({date: -1}) //sort by date desc
+        .sort({ date: -1 }) //sort by date desc
         .then(items => res.json(items));
 });
 
 // @route POST api/items
 // @desc create a item
-// @access public
+// @access private
 
-router.post('/', (req, res)=> {
+router.post('/', auth, (req, res) => {
     const newItem = new Item({
         name: req.body.name,
         quantity: req.body.quantity
@@ -28,12 +29,12 @@ router.post('/', (req, res)=> {
 
 // @route DELETE api/items
 // @desc delete a item
-// @access public
+// @access private
 
-router.delete('/:id', (req, res)=> {
+router.delete('/:id', auth, (req, res) => {
     Item.findById(req.params.id)
-        .then(item => item.remove().then(() => res.status(200).json({success: true})))
-        .catch(err=> res.status(404).json({success: false}));
+        .then(item => item.remove().then(() => res.status(200).json({ success: true })))
+        .catch(err => res.status(404).json({ success: false }));
 });
 
 module.exports = router;
